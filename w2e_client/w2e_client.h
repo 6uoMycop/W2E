09e960e2eed64292ad6f346adb51280dd6f478de
Petushkov Arkady <Arkady1234@yandex.ru>
2024-08-12 16:00:57 +0300

    Added IPv4/UDP incapsulation (mvp)

--- w2e_client/w2e_client.h
+++ w2e_client/w2e_client.h
@@ -14,4 +14,11 @@
 #include "w2e_common.h"
 
 
+/**
+ * "Preamble" is placed before packet and used for insertion of new IP + UDP header.
+ * Currently IPv4 without options + UDP
+ */
+#define W2E_PREAMBLE_SIZE 20 + 8
+
+
 #endif // __W2E_CLIENT_H
--- w2e_common/w2e_common.h
+++ w2e_common/w2e_common.h
@@ -28,6 +28,10 @@
 #endif // !W2E_HOST_MAXLEN
 
 #ifndef W2E_MAX_PACKET_SIZE
+/**
+ * Effective packet length is reduced because of insertion of new headers.
+ * Refer to client's and server's source code.
+ */
 #define W2E_MAX_PACKET_SIZE 9016
 #endif // !W2E_MAX_PACKET_SIZE
 
@@ -48,7 +52,7 @@
 #define W2E_VERBOSE
 #endif // !W2E_VERBOSE
 /** Define debug printf macro */
-#define w2e_dbg_printf(fmt, ...) printf("[DBG] %s() %s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
+#define w2e_dbg_printf(fmt, ...) printf("[DBG] %s()\t%s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
 #endif // W2E_DEBUG
 
 
@@ -60,12 +64,23 @@
 #define w2e_log_printf(...) do {} while (0);
 #else
 /** Define verbose printf macro */
-#define w2e_log_printf(fmt, ...) printf("[LOG] %s() %s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
+#define w2e_log_printf(fmt, ...) printf("[LOG] %s()\t%s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
 #endif // !W2E_VERBOSE
 
 
 /** Define error printf macro */
-#define w2e_print_error(fmt, ...) fprintf(stderr, "[ERROR] %s() %s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
+#define w2e_print_error(fmt, ...) fprintf(stderr, "[ERROR] %s()\t%s:%d:\t" fmt, __func__, __FILE__, __LINE__, ##__VA_ARGS__);
+
+
+/**
+ * For packed structures.
+ */
+#ifdef _MSC_VER
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
+#else
+#error NOT MSVC
+#endif
+
 
 
 HANDLE w2e_common__init(char* filter, UINT64 flags);
