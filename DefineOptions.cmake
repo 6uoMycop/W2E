dbfa60a98adfffddb64b075f1c7874b75c15e9cc
Petushkov Arkady <Arkady1234@yandex.ru>
2024-09-05 12:28:18 +0300

    copy lib

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -5,9 +5,6 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
 	add_compile_definitions(W2E_DEBUG)
 endif()
 
-# Unit tests enable
-set(UNIT_TESTING ON CACHE BOOL "CMocka: Build with unit testing" FORCE)
-
 add_compile_definitions(W2E_DEBUG_NO_HEX)
 add_compile_definitions(W2E_VERBOSE)
 
@@ -17,8 +14,9 @@ add_compile_definitions(W2E_MAX_CLIENTS=10)
 #add_compile_definitions(W2E_CT_SESSION_TTL=600)
 add_compile_definitions(W2E_CT_SESSION_TTL=100)
 
-# cmocka
-set(WITH_STATIC_LIB ON CACHE BOOL "CMocka: Build with a static library" FORCE)
-set(WITH_CMOCKERY_SUPPORT OFF CACHE BOOL "CMocka: Install a cmockery header" FORCE)
+
+# Unit tests enable
+set(W2E_UNIT_TESTING ON CACHE BOOL "W2E Unit tests enable")
+
+# CMocka
 set(WITH_EXAMPLES OFF CACHE BOOL "CMocka: Build examples" FORCE)
-set(PICKY_DEVELOPER OFF CACHE BOOL "CMocka: Build with picky developer flags" FORCE)
