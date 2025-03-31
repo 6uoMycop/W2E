ea2d1038c125c033730742a3563b5c9706ce3c45
6uoMycop <arkady1234@yandex.ru>
2025-04-01 02:03:54 +0300

    encoding

--- w2e_server/w2e_server.c
+++ w2e_server/w2e_server.c
@@ -98,15 +98,15 @@ static int __w2e_server__counters_init()
 		w2e_print_error("Error calling lseek()");
 		return -1;
 	}/* Something needs to be written at the end of the file to
-     * have the file actually have the new size.
-     * Just writing an empty string at the current file position will do.
-     *
-     * Note:
-     *  - The current position in the file is at the end of the stretched 
-     *    file due to the call to lseek().
-     *  - An empty string is actually a single '\0' character, so a zero-byte
-     *    will be written at the last byte of the file.
-     */
+	 * have the file actually have the new size.
+	 * Just writing an empty string at the current file position will do.
+	 *
+	 * Note:
+	 *  - The current position in the file is at the end of the stretched 
+	 *    file due to the call to lseek().
+	 *  - An empty string is actually a single '\0' character, so a zero-byte
+	 *    will be written at the last byte of the file.
+	 */
 	if (write(ctrs_shmm_fd, "", 1) != 1)
 	{
 		close(ctrs_shmm_fd);
