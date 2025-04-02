cce4511bc368197499d9529f727c9436355b493f
6uoMycop <arkady1234@yandex.ru>
2025-04-03 01:56:48 +0300

    w2e_get_counters.sh update
     human readable timestamp

--- misc/scripts_lin/w2e_get_counters.sh
+++ misc/scripts_lin/w2e_get_counters.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Parses shmm counters and prints in decimal. Reverts byte order.
+# Parses shmm counters and prints in decimal. '-e' reverts byte order.
 # Refer to w2e_ctrs_t declaration.
-#
-# -s 8 -- Cut off 64-bit timestamp
 
-xxd -c 4 -g 1 -s 8 /tmp/.w2e_ctrs_shmm.bin | awk '{print $5 $4 $3 $2}' | awk '{printf "%d\n", "0x" $1}'
+# Example output:
+# Wed Apr  2 22:29:02 UTC 2025
+# 58699
+# 58651
+# 0
+# 0
+# 48
+# 0
+# 22073
+# 19119
+
+
+# Timestamp 64-bit
+xxd -c 8 -g 8 -l 8 -e /tmp/.w2e_ctrs_shmm.bin | awk '{printf "%d\n", "0x" $2}' | date
+
+# All subsequent fields (32-bit)
+xxd -c 4 -g 4 -s 8 -e /tmp/.w2e_ctrs_shmm.bin | awk '{printf "%d\n", "0x" $2}'
